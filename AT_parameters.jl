0f9a4dffcef144b590cc4e555b6642f36335c596
FrancescaCrucinio <francesca.crucinio@warwick.ac.uk>
2020-03-24 13:20:50 +0000

    parallel

--- AT_parameters.jl
+++ AT_parameters.jl
@@ -1,4 +1,5 @@
 push!(LOAD_PATH, "C:/Users/Francesca/OneDrive/Desktop/WGF/myModules")
+# push!(LOAD_PATH, "C:/Users/francesca/Documents/GitHub/WGF/myModules")
 # Julia packages
 using Revise;
 using StatsPlots;
@@ -7,6 +8,7 @@ using Statistics;
 using StatsBase;
 using KernelEstimator;
 using Random;
+using JLD;
 # custom packages
 using diagnostics;
 using smcems;
@@ -38,7 +40,7 @@ lambda = [range(0, stop = 0.9, length = 10); range(1, stop = 9, length = 9);
             range(10, stop = 100, length = 10)];
 
 # number of repetitions
-Nrep = 1;
+Nrep = 1000;
 
 # diagnostics
 diagnosticsWGF = zeros(length(lambda), 5);
@@ -70,3 +72,9 @@ p5 = plot(lambda, diagnosticsWGF[:, 5], lw = 3, legend = false);
 title!("KL")
 p6 = plot()
 plot(p1, p2, p3, p4, p5, p6, layout = (2, 3))
+
+
+save("parameters.jld", "lambda", lambda, "diagnosticsWGF", diagnosticsWGF,
+        "Nparticles", Nparticles, "Niter", Niter)
+# save("C:/Users/francesca/Dropbox/parameters.jld", "lambda", lambda, "diagnosticsWGF", diagnosticsWGF,
+#         "Nparticles", Nparticles, "Niter", Niter)
