d7229e2bf779326a2bb6a6f3ed1bdf8c79719687
FrancescaCrucinio <francesca.crucinio@warwick.ac.uk>
2020-04-17 18:23:50 +0100

    rep rm

--- mvn.jl
+++ mvn.jl
@@ -35,15 +35,15 @@ h(x) = pdf.(MvNormal(mu, sigmaH), x);
 g(x, y) = pdf.(MvNormal(x, sigmaG), y);
 
 # number of iterations
-Niter = trunc(Int, 1e04);
+Niter = trunc(Int, 1e03);
 # samples from h(y)
-M = 10000;
+M = 1000;
 # values at which evaluate KDE
 KDEx = range(0, stop = 1, length = 1000);
 # number of particles
-Nparticles = 10000;
+Nparticles = 100;
 # regularisation parameter
-lambda = 10;
+lambda = 100;
 
 # initial distribution
 x0 = 6*rand(2, Nparticles) .- 3;
@@ -52,15 +52,15 @@ x, y = wgf_mvnormal(Nparticles, Niter, lambda, x0, M, mu, sigmaH, sigmaG);
 p1 = scatter(x[Niter, :], y[Niter, :])
 
 
-sample = rand(MvNormal(mu, sigmaF), 100000);
-p2 = scatter(sample[1, :], sample[2, :])
-sampleH = rand(MvNormal(mu, sigmaH), 100000);
-p3 = scatter(sampleH[1, :], sampleH[2, :])
-plot(p1, p2, p3, layout =(1, 3))
-
-
-KDEyWGF =  KernelDensity.kde((x[end, :], y[end, :]));
-Xbins = range(-0.5, stop = 1.5, length = 1000);
-Ybins = range(-1, stop = 2, length = 2000);
-res = pdf(KDEyWGF, Ybins, Xbins);
-heatmap(Xbins, Ybins, res)
+# sample = rand(MvNormal(mu, sigmaF), 100000);
+# p2 = scatter(sample[1, :], sample[2, :])
+# sampleH = rand(MvNormal(mu, sigmaH), 100000);
+# p3 = scatter(sampleH[1, :], sampleH[2, :])
+# plot(p1, p2, p3, layout =(1, 3))
+#
+#
+# KDEyWGF =  KernelDensity.kde((x[end, :], y[end, :]));
+# Xbins = range(-0.5, stop = 1.5, length = 1000);
+# Ybins = range(-1, stop = 2, length = 2000);
+# res = pdf(KDEyWGF, Ybins, Xbins);
+# heatmap(Xbins, Ybins, res)
