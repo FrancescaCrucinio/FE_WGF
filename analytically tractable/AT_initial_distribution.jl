46e2dd680099157e34f329830e581fb837204650
FrancescaCrucinio <francesca.crucinio@warwick.ac.uk>
2020-05-20 22:48:26 +0100

    at initial done

--- analytically tractable/AT_initial_distribution.jl
+++ analytically tractable/AT_initial_distribution.jl
@@ -112,20 +112,34 @@ labels = [L"$\delta_0$" L"$\delta_{0.5}$" L"$\delta_1$" L"U$[0, 1]$" L"$N(m, \si
 p1 = StatsPlots.plot(2:Niter, m, lw = 3, label = labels, xlabel=L"Iteration",
     ylabel=L"$\hat{m}_t$", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
 p2 = StatsPlots.plot(2:Niter, v, lw = 3, label = labels, xlabel=L"Iteration",
-    ylabel=L"$\hat{v}_t$", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
+    ylabel=L"$\hat{\sigma}_t$", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
 p3 = StatsPlots.plot(2:Niter, q, lw = 3, label = labels, xlabel=L"Iteration",
     ylabel=L"$MSE_{95}$", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
-p4 = StatsPlots.plot(2:Niter, misef, lw = 3, label = labels, xlabel=L"Iteration",
+p4 = StatsPlots.plot(10:Niter, misef[9:end, :], lw = 3, label = labels, xlabel=L"Iteration",
     ylabel=L"MISE", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
-p5 = StatsPlots.plot(2:Niter, E, lw = 3, label = labels, xlabel=L"Iteration",
+p5 = StatsPlots.plot(10:Niter, E[9:end, :], lw = 3, label = labels, xlabel=L"Iteration",
     ylabel=L"$E(\rho_t)$", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
 p6 = StatsPlots.plot(2:Niter, KL, lw = 3, label = labels, xlabel=L"Iteration",
     ylabel=L"KL", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
 p7 = StatsPlots.plot(2:Niter, ent, lw = 3, label = labels, xlabel=L"Iteration",
     ylabel=L"$ent(\rho_t)$", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
-plot(p1, p2, p3, p4, p5, p6, layout = (3, 2))
-#
-# savefig(p2, "initial_distribution_variance.pdf")
-# savefig(p3, "initial_distribution_mse.pdf")
-# savefig(p4, "initial_distribution_mise.pdf")
-# savefig(p5, "initial_distribution_E.pdf")
+
+# (iteration 1 not present)
+p2short = StatsPlots.plot(5:100, v[4:99, :], lw = 3, label = labels, xlabel=L"Iteration",
+    ylabel=L"$\hat{\sigma}_t$", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
+p3short = StatsPlots.plot(5:100, q[4:99, :], lw = 3, label = labels, xlabel=L"Iteration",
+    ylabel=L"$MSE_{95}$", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
+p4short = StatsPlots.plot(5:100, misef[4:99, :], lw = 3, label = labels, xlabel=L"Iteration",
+    ylabel=L"MISE", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
+p5short = StatsPlots.plot(5:100, E[4:99, :], lw = 3, label = labels, xlabel=L"Iteration",
+    ylabel=L"$E(\rho_t)$", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
+p7short = StatsPlots.plot(5:100, ent[4:99, :], lw = 3, label = labels, xlabel=L"Iteration",
+    ylabel=L"$ent(\rho_t)$", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
+
+savefig(p2short, "initial_distribution_variance.pdf")
+savefig(p4short, "initial_distribution_mise.pdf")
+savefig(p5short, "initial_distribution_E.pdf")
+savefig(p7short, "initial_distribution_ent.pdf")
+
+savefig(p4, "initial_distribution_mise_1000iter.pdf")
+savefig(p5, "initial_distribution_E_1000iter.pdf")
