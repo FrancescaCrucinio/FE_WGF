e0df64a82beaa31a9ced26527e47a2a048b8fa3e
FrancescaCrucinio <francesca.crucinio@warwick.ac.uk>
2020-05-23 20:09:44 +0100

    parameters 500/1000

--- analytically tractable/AT_parameters_plot.jl
+++ analytically tractable/AT_parameters_plot.jl
@@ -9,25 +9,26 @@ using LaTeXStrings;
 
 
 lambda = load("analytically tractable/parameters1000.jld", "lambda");
-diagnosticsWGF = load("analytically tractable/parameters1000.jld", "diagnosticsWGF");
+diagnosticsWGF1000 = load("analytically tractable/parameters1000.jld", "diagnosticsWGF");
+diagnosticsWGF500 = load("analytically tractable/parameters500.jld", "diagnosticsWGF");
 
 pyplot()
-p1 = plot(lambda, diagnosticsWGF[:, 1], lw = 3, legend = false,
+p1 = plot(lambda, [diagnosticsWGF500[:, 1] diagnosticsWGF1000[:, 1]], lw = 3, legend = false,
         xlabel="lambda", ylabel="mean");
 hline!([0.5]);
-p2 = plot(lambda, diagnosticsWGF[:, 2], lw = 3, legend = false,
+p2 = plot(lambda, [diagnosticsWGF500[:, 2] diagnosticsWGF1000[:, 2]], lw = 3, legend = false,
         xlabel="lambda", ylabel="variance");
 hline!([0.043^2]);
-p3 = plot(lambda, diagnosticsWGF[:, 3], lw = 3, legend = false,
+p3 = plot(lambda, [diagnosticsWGF500[:, 3] diagnosticsWGF1000[:, 3]], lw = 3, legend = false,
         xlabel="lambda", ylabel="95th MSE");
-p4 = plot(lambda, diagnosticsWGF[:, 4], lw = 3, legend = false,
+p4 = plot(lambda, [diagnosticsWGF500[:, 4] diagnosticsWGF1000[:, 4]], lw = 3, legend = false,
         xlabel="lambda", ylabel="MISE");
-p5 = plot(lambda, diagnosticsWGF[:, 5], lw = 3, legend = false,
+p5 = plot(lambda, [diagnosticsWGF500[:, 5] diagnosticsWGF1000[:, 5]], lw = 3, legend = false,
         xlabel="lambda", ylabel="E(rho)");
-p6 = plot(lambda, diagnosticsWGF[:, 6], lw = 3, legend = false,
+p6 = plot(lambda, [diagnosticsWGF500[:, 6] diagnosticsWGF1000[:, 6]], lw = 3, legend = false,
         xlabel="lambda", ylabel="entropy");
 
-p7 = plot(lambda, diagnosticsWGF[:, 7], lw = 3, legend = false,
+p7 = plot(lambda, [diagnosticsWGF500[:, 7] diagnosticsWGF1000[:, 7]], lw = 3, legend = false,
         xlabel="lambda", ylabel="kl");
 
 plot(p1, p2, p3, p4, p5, p6, layout = (2, 3))
