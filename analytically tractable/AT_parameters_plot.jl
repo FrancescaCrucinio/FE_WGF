ed7d76e8468ca2ac49be51b8b82d389f14a61e05
FrancescaCrucinio <francesca.crucinio@warwick.ac.uk>
2020-05-27 18:12:31 +0100

    parameters plot

--- analytically tractable/AT_parameters_plot.jl
+++ analytically tractable/AT_parameters_plot.jl
@@ -12,30 +12,28 @@ lambda = load("analytically tractable/parameters1000.jld", "lambda");
 diagnosticsWGF1000 = load("analytically tractable/parameters1000.jld", "diagnosticsWGF");
 diagnosticsWGF500 = load("analytically tractable/parameters500.jld", "diagnosticsWGF");
 
+labels = ["N=500" "N=1000"];
 pyplot()
-p1 = plot(lambda, [diagnosticsWGF500[:, 1] diagnosticsWGF1000[:, 1]], lw = 3, legend = false,
-        xlabel="lambda", ylabel="mean");
-hline!([0.5]);
-p2 = plot(lambda, [diagnosticsWGF500[:, 2] diagnosticsWGF1000[:, 2]], lw = 3, legend = false,
-        xlabel="lambda", ylabel="variance");
-hline!([0.043^2]);
-p3 = plot(lambda, [diagnosticsWGF500[:, 3] diagnosticsWGF1000[:, 3]], lw = 3, legend = false,
-        xlabel="lambda", ylabel="95th MSE");
-p4 = plot(lambda, [diagnosticsWGF500[:, 4] diagnosticsWGF1000[:, 4]], lw = 3, legend = false,
-        xlabel="lambda", ylabel="MISE");
-p5 = plot(lambda, [diagnosticsWGF500[:, 5] diagnosticsWGF1000[:, 5]], lw = 3, legend = false,
-        xlabel="lambda", ylabel="E(rho)");
-p6 = plot(lambda, [diagnosticsWGF500[:, 6] diagnosticsWGF1000[:, 6]], lw = 3, legend = false,
-        xlabel="lambda", ylabel="entropy");
-
-p7 = plot(lambda, [diagnosticsWGF500[:, 7] diagnosticsWGF1000[:, 7]], lw = 3, legend = false,
-        xlabel="lambda", ylabel="kl");
+p1 = plot(lambda, [diagnosticsWGF500[:, 1] diagnosticsWGF1000[:, 1]], lw = 3, label = labels,
+        xlabel=L"$\alpha$", ylabel=L"$\hat{m}_t$", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
+hline!([0.5], label="True mean");
+p2 = plot(lambda, [diagnosticsWGF500[:, 2] diagnosticsWGF1000[:, 2]], lw = 3, label = labels,
+        xlabel=L"$\alpha$", ylabel=L"$\hat{\sigma}_t$", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
+hline!([0.043^2], label =  "True variance");
+p3 = plot(lambda, [diagnosticsWGF500[:, 3] diagnosticsWGF1000[:, 3]], lw = 3, label = labels,
+        xlabel=L"$\alpha$", ylabel=L"$MSE_{95}$", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
+p4 = plot(lambda, [diagnosticsWGF500[:, 4] diagnosticsWGF1000[:, 4]], lw = 3, label = labels,
+        xlabel=L"$\alpha$", ylabel=L"MISE", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
+p5 = plot(lambda, [diagnosticsWGF500[:, 5] diagnosticsWGF1000[:, 5]], lw = 3, label = labels,
+        xlabel=L"$\alpha$", ylabel=L"$E(\rho_t)$", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
+p6 = plot(lambda, [diagnosticsWGF500[:, 6] diagnosticsWGF1000[:, 6]], lw = 3, label = labels,
+        xlabel=L"$\alpha$", ylabel=L"$ent(\rho_t)$", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
+p7 = plot(lambda, [diagnosticsWGF500[:, 7] diagnosticsWGF1000[:, 7]], lw = 3, label = labels,
+        xlabel=L"$\alpha$", ylabel=L"KL", xguidefontsize=10, yguidefontsize=10, legendfontsize=10);
 
 plot(p1, p2, p3, p4, p5, p6, layout = (2, 3))
 
-# savefig(p1, "mean1000.pdf")
-# savefig(p2, "var1000.pdf")
-# savefig(p3, "mse1000.pdf")
-# savefig(p4, "mise1000.pdf")
-# savefig(p5, "e1000.pdf")
-# savefig(p6, "entropy1000.pdf")
+savefig(p2, "parameters_var.pdf")
+savefig(p4, "parameters_mise.pdf")
+savefig(p5, "parameters_E.pdf")
+savefig(p6, "parameters_ent.pdf")
