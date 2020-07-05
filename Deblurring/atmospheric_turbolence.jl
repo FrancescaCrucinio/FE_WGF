4c37a05e3703e2e225aeafd1742175bf901c42ff
FrancescaCrucinio <32672497+FrancescaCrucinio@users.noreply.github.com>
2020-07-05 22:17:41 +0100

    save

--- Deblurring/atmospheric_turbolence.jl
+++ Deblurring/atmospheric_turbolence.jl
@@ -29,33 +29,10 @@ M = 20000;
 Nparticles = 20000;
 # regularisation parameter
 lambda = 0.01;
-dt = 10^-3;
+dt = 10^-4;
 
 R = 50;
 beta = 3;
 x, y = wgf_turbolence(Nparticles, Niter, dt, lambda, Imageh, M, beta, R);
 
-Xbins = range(-1 + 1/pixels[2], stop = 1 - 1/pixels[2], length = pixels[2]);
-Ybins = range(0.5 - 1/pixels[1], stop = -0.5 + 1/pixels[1], length = pixels[1]);
-KDEyWGF =  KernelDensity.kde((y[end, :], x[end, :]));
-resWGF = pdf(KDEyWGF, Ybins, Xbins);
-
-# normalize image
-resWGF = map(clamp01nan, resWGF);
-
-
-save("resWGF10-3.jld", "resWGF", resWGF)
-
-Gray.(resWGF)
-Gray.(Imageh)
-
-Imagef = load("Deblurring/galaxy.png");
-Imagef = Gray.(Imagef);
-Imagef
-Imagef = convert(Array{Float64}, Imagef);
-
-d = Euclidean()
-d(Gray.(resWGF), Gray.(Imagef))
-
-(norm(resWGF - Imagef).^2)/length(resWGF)
-save("galaxy_reconstruction.png", Gray.(resWGF));
+save("turbolence05072020.jld", "x", x, "y", y, "Niter", Niter, "pixels", pixels);
