4665a2e404749ab7e9ae8a575b5979bead81ab10
FrancescaCrucinio <francesca.crucinio@warwick.ac.uk>
2020-07-10 18:18:01 +0100

    plots

--- analytically tractable/gradient.jl
+++ analytically tractable/gradient.jl
@@ -1,30 +1,31 @@
 using Distributions;
 using Plots;
+using Random;
 include("drift_exact.jl")
 include("drift_approximate.jl")
+
+# set seed
+Random.seed!(1234);
 
 x = range(0, 1, length = 1000);
 sigmaG = 0.045^2;
 sigmaF = 0.043^2;
 sigmaH = sigmaF + sigmaG;
 sigma0 = 0.01;
-M = 5;
+
 g(x, y) = (y .- x)./sigmaG .* pdf.(Normal.(x, sqrt(sigmaG)), y);
-y = rand(Normal(0.5, sqrt(sigmaH)), M);
 f(x) = pdf.(Normal(0.5, sqrt(sigmaF)), x);
-p1 = plot(f, 0, 1, legend = false)
-title!("f")
-gradient = zeros(M, 1000);
-p2 = plot()
-for i=1:M
-    gradient[i, :] = g(x, y[i]);
-    plot!(p2, x, gradient[i, :])
-end
-title!("gradient")
+
 driftE = drift_exact(0.5, sigma0, sigmaG, sigmaH, x);
 p3 = plot(x, driftE)
 title!("exact drift")
-driftA = drift_approximate(0.5, sigma0, sigmaG, sigmaH, x, 1000);
+
+N = [100 500 1000 5000 10000];
+M = 1000;
+driftA = zeros(length(x), length(N));
+for i=1:length(N)
+    driftA[:, i] = drift_approximate(0.5, sigma0, sigmaG, sigmaH, x, M, N[i]);
+end
 p4 = plot(x, driftA)
 title!("approximate drift")
-plot(p1, p2, p3, p4, layout=(4, 1))
+plot(p3, p4, layout=(2, 1))
