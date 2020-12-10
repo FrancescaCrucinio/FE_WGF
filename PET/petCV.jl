47f12a7b6f46fdb4c271ae5fa8814071ef1d77af
FrancescaCrucinio <francesca.crucinio@warwick.ac.uk>
2020-12-10 08:29:06 +0000

    petcV

--- PET/petCV.jl
+++ PET/petCV.jl
@@ -10,7 +10,7 @@ using Random;
 using LinearAlgebra;
 using DelimitedFiles;
 using Interpolations;
-using JLD;
+using Distances;
 # R
 using RCall;
 @rimport ks as rks;
@@ -34,74 +34,82 @@ phi_angle = range(0, stop = 2*pi, length = nphi);
 # number of offsets
 offsets = floor(size(sinogram, 1)/2);
 xi = range(-offsets, stop = offsets, length = size(sinogram, 1));
+xi = xi/maximum(xi);
 
-# sample from μ
-muSample = histogram2D_sampler(sinogram, phi_angle, xi, 10^6);
-# dt and number of iterations
-dt = 1e-02;
-Niter = 200;
-# samples from h(y)
-M = 5000;
-# number of particles
-Nparticles = 5000;
-# regularisation parameter
-alpha = range(0.001, stop = 1, length = 10);
-# variance of normal describing alignment
-sigma = 0.02;
+# grid (use only 2500 points to compute KL)
+X1bins = range(-0.75+ 1/pixels[1], stop = 0.75 - 1/pixels[1], length = 50);
+X2bins = range(-0.75 + 1/pixels[2], stop = 0.75 - 1/pixels[2], length = 50);
+gridX1 = repeat(X1bins, inner=[50, 1]);
+gridX2 = repeat(X2bins, outer=[50 1]);
+KDEeval = [gridX1 gridX2];
 
 # function computing KDE
-# grid
-Xbins = range(-0.75+ 1/pixels[1], stop = 0.75 - 1/pixels[1], length = pixels[1]);
-Ybins = range(-0.75 + 1/pixels[2], stop = 0.75 - 1/pixels[2], length = pixels[2]);
-gridX = repeat(Xbins, inner=[pixels[2], 1]);
-gridY = repeat(Ybins, outer=[pixels[1] 1]);
-KDEeval = [gridX gridY];
 function phi(t)
     RKDE = rks.kde(x = [t[1:Nparticles] t[(Nparticles+1):(2Nparticles)]], var"eval.points" = KDEeval);
     return abs.(rcopy(RKDE[3]));
 end
-a = 1;
-# function computing E
-function psi(t)
+# function computing entropy
+function psi_ent(t)
     # entropy
     function remove_non_finite(x)
 	       return isfinite(x) ? x : 0
     end
     ent = -mean(remove_non_finite.(t .* log.(t)));
+end
+# function computing KL
+function psi_kl(t)
+    # # entropy
+    # function remove_non_finite(x)
+	#        return isfinite(x) ? x : 0
+    # end
+    # ent = -mean(remove_non_finite.(t .* log.(t)));
     # kl
-    trueH = @rget sinogram;
-    refY1 = @rget phi_angle;
-    refY2 = @rget xi;
+    trueMu = sinogram;
+    refY1 = phi_angle;
+    refY2 = xi;
     # approximated value
     delta1 = refY1[2] - refY1[1];
     delta2 = refY2[2] - refY2[1];
-    hatH = zeros(length(refY2), length(refY1));
-    # convolution with approximated f
+    hatMu = zeros(length(refY2), length(refY1));
+    # convolution with approximated ρ
     # this gives the approximated value
     for i=1:length(refY2)
         for j=1:length(refY1)
-            hatH[i, j] = delta1*delta2*sum(pdf.(Normal.(0, sigma), KDEeval[:, 1] * cos(refY1[j]) .+
+            hatMu[i, j] = delta1*delta2*sum(pdf.(Normal.(0, sigma), KDEeval[:, 1] * cos(refY1[j]) .+
                 KDEeval[:, 2] * sin(refY1[j]) .- refY2[i]).*t);
         end
     end
-    kl = kl_divergence(trueH[:], hatH[:]);
-    return kl-a*ent;
+    kl = kl_divergence(trueMu[:], hatMu[:]);
+    return kl;
 end
 
-# divide muSample into groups
+# WGF
+# dt and number of iterations
+dt = 1e-02;
+Niter = 100;
+# samples from h(y)
+M = 10000;
+# number of particles
+Nparticles = 10000;
+# regularisation parameter
+alpha = range(0.0001, stop = 1, length = 10);
+# variance of normal describing alignment
+sigma = 0.02;
+# sample from μ
+muSample = histogram2D_sampler(sinogram, phi_angle, xi, 10^6);
+
 L = 10;
-muSample = reshape(muSample, (L, Int(length(muSample)/L)));
+
 E = zeros(length(alpha), L);
-Threads.@threads for i=1:length(alpha)
-    @simd for l=1:L
+for i=1:length(alpha)
+    for l=1:L
         # get reduced sample
-        muSampleL = muSample[1:end .!= l, :];
+        muSampleL = muSample[:, setdiff(1:100000, Tuple(((1:10000) .+ (l-1)*10000)))];
         # WGF
-        x, y = wgf_pet_tamed(Nparticles, dt, Niter, alpha[i], muSampleL, M, phi_angle, xi, sigma, 0.5);
+        x1, x2 = wx1, x2 = wgf_pet_tamed(Nparticles, dt, Niter, alpha[i], muSampleL, M, sigma, 0.5);
         # KL
-        a = alpha[i];
-        KDE = phi([x[Niter, :], y[Niter, :]]);
-        E[i, l] = psi(KDE);
+        KDE = phi([x1[Niter, :]; x2[Niter, :]]);
+        E[i, l] = psi_kl(KDE) - alpha[i]*psi_ent(KDE);
         println("$i, $l")
     end
 end
