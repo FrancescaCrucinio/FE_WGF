79ea3adf27fe286461a5f2f377565a10e3087236
FrancescaCrucinio <f.crucinio@warwick.ac.uk>
2021-03-16 09:06:04 +0000

    Update ct.jl

--- CT/ct.jl
+++ CT/ct.jl
@@ -12,7 +12,7 @@ using DelimitedFiles;
 using Interpolations;
 using Images;
 using Distances;
-using KernelDensity;
+using KernelDensityEstimate;
 # R
 using RCall;
 R"""
@@ -47,22 +47,21 @@ gridX1 = repeat(X1bins, inner=[pixels[2], 1]);
 gridX2 = repeat(X2bins, outer=[pixels[1] 1]);
 KDEeval = [gridX1 gridX2];
 
-######## rewrite the function below -- we have the empirical measure not the sample!!!
-
-
 # functional approximation
-function psi(piSample)
-    loglik = zeros(1, size(muSample, 1));
-    for i=1:size(muSample, 1)
-        loglik[i] = mean(pdf.(Normal.(0, sigma), piSample[:, 1] * cos(muSample[i, 1]) .+
-            piSample[:, 2] * sin(muSample[i, 1]) .- muSample[i, 2]));
+function psi(t)
+    piSample = [transpose(t[1:Nparticles]); transpose(t[(Nparticles+1):(2Nparticles)])];
+    loglik = zeros(size(sinogram));
+    for i=1:nphi
+        for j=1:length(xi)
+        loglik[j, i] = mean(pdf.(Normal.(0, sigma), piSample[1, :] * cos(phi_angle[i]) .+
+            piSample[2, :] * sin(phi_angle[i]) .- xi[j]));
+        end
     end
     loglik = -log.(loglik);
-    kl = mean(loglik);
-    prior = pdf.(MvNormal(m0, sigma0), piSample);
-    piKDE = kde((piSample[:, 1],  piSample[:, 2]));
-    pihat = pdf(piKDE, piSample[:, 1], piSample[:, 2]);
-    pihat = abs.(pihat[:]);
+    kl = (phi_angle[2] - phi_angle[1])*(xi[2]-xi[1])*sum(loglik);
+    prior = pdf(MvNormal(m0, Diagonal(sigma0)), piSample);
+    piKDE = kde!(piSample);
+    pihat = piKDE(piSample);
     kl_prior = mean(log.(pihat./prior));
     return kl+alpha*kl_prior;
 end
@@ -90,3 +89,6 @@ sigma = 0.02;
 tWGF = @elapsed begin
 x1, x2 = wgf_ct_tamed(Nparticles, dt, Niter, alpha, x0, m0, sigma0, M, sinogram, phi_angle, xi, sigma);
 end
+
+EWGF = mapslices(psi, [x1 x2], dims = 2);
+plot(EWGF)
