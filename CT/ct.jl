4dcdd88f3789a8930fcfcaff0c42f666e58de1b6
FrancescaCrucinio <f.crucinio@warwick.ac.uk>
2021-04-15 18:58:53 +0100

    ct

--- CT/ct.jl
+++ CT/ct.jl
@@ -15,9 +15,10 @@ using KernelDensity;
 using TomoForward;
 using XfromProjections;
 # custom packages
-using wgf_prior;
 using samplers;
-include("ct_kde.jl")
+using wgf_prior;
+
+
 # set seed
 Random.seed!(1234);
 
@@ -36,19 +37,21 @@ offsets = 729;
 proj_geom = ProjGeom(1.0, offsets, phi_angle);
 xi = range(-floor(offsets/2), stop = floor(offsets/2), length = offsets);
 xi = xi/maximum(xi);
+#
+# # sinogram
+# A = fp_op_parallel2d_line(proj_geom, pixels, pixels);
+# sinogram = A * vec(CTscan);
+# sinogram = reshape(Array(sinogram), (:, offsets));
+# sinogram = sinogram./maximum(sinogram);
+# save("sinogram.png", colorview(Gray, sinogram));
 
-# sinogram
-A = fp_op_parallel2d_line(proj_geom, pixels, pixels);
-sinogram = A * vec(CTscan);
-sinogram = reshape(Array(sinogram), (:, offsets));
-q = filter_proj(sinogram);
-sinogram = sinogram./maximum(sinogram);
-Gray.(sinogram)
-
-# filtered back projection
-fbp = A' * vec(q) .* (pi / nphi);
-fbp_img = reshape(fbp, size(CTscan));
-Gray.(fbp_img./maximum(fbp_img))
+sinogram = load("CT/sinogram.png");
+sinogram = convert(Array{Float64}, sinogram);
+# # filtered back projection
+# q = filter_proj(sinogram);
+# fbp = A' * vec(q) .* (pi / nphi);
+# fbp_img = reshape(fbp, size(CTscan));
+# Gray.(fbp_img./maximum(fbp_img))
 
 # grid
 X1bins = range(-1 + 1/pixels, stop = 1 - 1/pixels, length = pixels);
@@ -56,9 +59,9 @@ X2bins = range(-1 + 1/pixels, stop = 1 - 1/pixels, length = pixels);
 
 # parameters for WGF
 # number of particles
-Nparticles = 500;
+Nparticles = 1000;
 # number of samples from μ to draw at each iteration
-M = 500;
+M = 1000;
 # time discretisation
 dt = 1e-2;
 # number of iterations
@@ -70,7 +73,7 @@ m0 = [0; 0];
 sigma0 = [0.2; 0.2];
 # initial distribution
 x0 = sigma0[1]*randn(2, Nparticles);
-x0 = rand(2, Nparticles);
+#x0 = rand(2, Nparticles);
 # variance of normal describing alignment
 sigma = 0.02;
 
