31e20dc37043eaca31332b73607895f543b25f12
FrancescaCrucinio <f.crucinio@warwick.ac.uk>
2021-06-09 09:20:30 +0100

    clean up

--- Old stuff/drift_exact.jl
+++
@@ -1,6 +0,0 @@
-function drift_exact(μ, sigma0, sigmaG, sigmaH, x)
-    drift = (sigmaG + sigma0 - sigmaH)*(sigmaG + sigma0)/
-            (sigmaG^2 + sigmaG*sigma0 + sigma0*sigmaH)^(3/2) *
-            exp.(-(sigmaG + sigma0 - sigmaH)*(μ .- x).^2 ./
-            (2*(sigmaG^2 + sigmaG*sigma0 + sigma0*sigmaH))) .* (μ .- x);
-end
