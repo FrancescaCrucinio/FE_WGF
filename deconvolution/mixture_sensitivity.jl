bafcd3e7e81f276b8fada9b63738895f2265f24b
Francesca Crucinio <francescacrucinio@Francescas-MacBook-Pro.local>
2021-12-23 09:52:09 +0100

    misture

--- deconvolution/mixture_sensitivity.jl
+++ deconvolution/mixture_sensitivity.jl
@@ -1,3 +1,4 @@
+push!(LOAD_PATH, "C:/Users/Francesca/Desktop/WGF/MyModules")
 push!(LOAD_PATH, "/Users/francescacrucinio/Documents/WGF/myModules")
 # Julia packages
 using Revise;
@@ -6,12 +7,12 @@ using Distributions;
 using Statistics;
 using StatsBase;
 using Random;
+using Distances;
 using RCall;
 @rimport ks as rks
 # custom packages
 using wgf_prior;
 using samplers;
-using smcems;
 
 # set seed
 Random.seed!(1234);
@@ -23,42 +24,45 @@ mu(x) = 2*pdf.(Normal(0.3, sqrt(0.043^2 + 0.045^2)), x)/3 +
 K(x, y) = pdf.(Normal(x, 0.045), y);
 sdK = 0.045;
 
+# values at which evaluate KDE
+KDEx = range(0, stop = 1, length = 1000);
+
+
+
 # functional approximation
 function psiWGF(piSample, a, m0, sigma0, muSample)
-    loglik = zeros(1, length(muSample));
-    for i=1:length(muSample)
-        loglik[i] = mean(K.(piSample, muSample[i]));
-    end
-    loglik = -log.(loglik);
-    kl = mean(loglik);
-    prior = pdf.(Normal(m0, sigma0), piSample);
-    Rpihat = rks.kde(x = piSample, var"eval.points" = piSample);
+	# build KDE
+	Rpihat = rks.kde(x = piSample, var"eval.points" = KDEx);
     pihat = abs.(rcopy(Rpihat[3]));
-    kl_prior = mean(log.(pihat./prior));
+	# kl
+	trueMu = 2*pdf.(Normal(0.3, sqrt(0.043^2 + 0.045^2)), KDEx)/3 +
+			pdf.(Normal(0.5, sqrt(0.015^2 + 0.045^2)), KDEx)/3;
+	# approximated value
+	delta = KDEx[2] - KDEx[1];
+	hatMu = zeros(1, length(KDEx));
+	# convolution with approximated f
+	# this gives the approximated value
+	for i=1:length(KDEx)
+		hatMu[i] = delta*sum(pdf.(Normal.(KDEx, sdK), KDEx[i]).*pihat);
+	end
+	kl = kl_divergence(trueMu, hatMu);
+    # regularization term
+    prior = pdf.(Normal(m0, sigma0), KDEx);
+    kl_prior = kl_divergence(pihat, prior);
     return kl+a*kl_prior;
-end
-function psiSMC(piSample, W, muSample)
-    loglik = zeros(1, length(muSample));
-    for i=1:length(muSample)
-        loglik[i] = sum(W .* K.(piSample, muSample[i]));
-    end
-    loglik = -log.(loglik);
-    kl = mean(loglik);
-    return kl;
 end
 
 # parameters
 # dt and number of iterations
 dt = 1e-03;
-Niter = 100;
+Niter = 200;
 # number of particles
-Nparticles = 10000;
+Nparticles = 500;
 # regularisation parameters
-alpha = range(0, stop = 0.007, length = 10);
-epsilon = range(0, stop = 0.007, length = 10);
+alpha = range(0, stop = 0.1, length = 10);
 L = 10;
 EWGF = zeros(length(alpha), L);
-ESMC = zeros(length(alpha), L);
+
 for i=1:length(alpha)
     for l=1:L
         muSample = Ysample_gaussian_mixture(10^3);
@@ -72,12 +76,8 @@ for i=1:length(alpha)
         xWGF = wgf_DKDE_tamed(Nparticles, dt, Niter, alpha[i], x0, m0, sigma0, muSample, M, sdK);
         # estimate functional
         EWGF[i, l] = psiWGF(xWGF[Niter, :], alpha[i], m0, sigma0, muSample);
-        # SMCEMS
-        xSMC, W = smc_gaussian_mixture(Nparticles, Niter, epsilon[i], x0, muSample, M);
-        # estimate functional
-        ESMC[i, l] = psiSMC(xSMC[Niter, :], W[Niter, :], muSample);
         println("$i, $l")
     end
 end
 p = plot(alpha,  mean(EWGF, dims = 2), lw = 3, tickfontsize = 15)
-p = plot(epsilon,  mean(ESMC, dims = 2), lw = 3, tickfontsize = 15)
+# savefig(p,"mixture_sensitivity_E.pdf")
